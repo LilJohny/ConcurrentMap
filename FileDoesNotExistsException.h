aea5987606d79b963eb2bfee024e5877ba4f80df
Denis <mister.denis67808@gmail.com>
2020-04-30 00:47:09 +0300

    UPD

--- FileDoesNotExistsException.h
+++ FileDoesNotExistsException.h
@@ -1,8 +1,14 @@
-//
-// Created by denis on 30.04.20.
-//
-
 #ifndef AKSLAB5_FILEDOESNOTEXISTSEXCEPTION_H
 #define AKSLAB5_FILEDOESNOTEXISTSEXCEPTION_H
+
+#include <string>
+#include <stdexcept>
+
+struct FileDoesNotExistsException : public std::runtime_error {
+public:
+    explicit FileDoesNotExistsException(const std::string &filename) : runtime_error(
+            filename + " does not exists!") {}
+};
+
 
 #endif //AKSLAB5_FILEDOESNOTEXISTSEXCEPTION_H
