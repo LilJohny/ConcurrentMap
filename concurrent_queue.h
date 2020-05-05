b459c50b5829868a1f7f01e1a1e36f2457b2b55a
Denis Ivanenko <mister.denis67808@gmail.com>
2020-05-05 06:40:36 +0300

    Formatting

--- concurrent_queue.h
+++ concurrent_queue.h
@@ -7,47 +7,48 @@
 
 template<typename T>
 class concurrent_queue {
-public:
-    explicit concurrent_queue(size_t max_size) : max_size(max_size) {}
+ public:
+  explicit concurrent_queue(size_t max_size) : max_size(max_size) {}
 
-    void push_back(T value) {
-        std::unique_lock<std::mutex> lg{mutex};
-        cv_not_full.wait(lg, [this]() { return queue.size() != max_size; });
-        queue.push_back(value);
-        cv_not_empty.notify_one();
-    }
-    void push_front(T value) {
-        std::unique_lock<std::mutex> lg{mutex};
-        cv_not_full.wait(lg, [this]() { return queue.size() != max_size; });
-        queue.push_front(value);
-        cv_not_empty.notify_one();
-    }
+  void push_back(T value) {
+    std::unique_lock<std::mutex> lg{mutex};
+    cv_not_full.wait(lg, [this]() { return queue.size() != max_size; });
+    queue.push_back(value);
+    cv_not_empty.notify_one();
+  }
+  void push_front(T value) {
+    std::unique_lock<std::mutex> lg{mutex};
+    cv_not_full.wait(lg, [this]() { return queue.size() != max_size; });
+    queue.push_front(value);
+    cv_not_empty.notify_one();
+  }
 
-    size_t size() {
-        std::unique_lock<std::mutex> lock(mutex);
-        return queue.size();
-    }
+  size_t size() {
+    std::unique_lock<std::mutex> lock(mutex);
+    return queue.size();
+  }
 
-    T pop() {
-        std::unique_lock<std::mutex> lg{mutex};
-        cv_not_empty.wait(lg, [this]() { return queue.size() != 0; });
-        T value = queue.front();
-        queue.pop_front();
-        cv_not_full.notify_one();
-        return value;
-    }
-    T front(){
-        std::unique_lock<std::mutex> lg{mutex};
-        cv_not_empty.wait(lg, [this]() { return queue.size() != 0; });
-        T value = queue.front();
-        return value;
-    }
+  T pop() {
+    std::unique_lock<std::mutex> lg{mutex};
+    cv_not_empty.wait(lg, [this]() { return queue.size() != 0; });
+    T value = queue.front();
+    queue.pop_front();
+    cv_not_full.notify_one();
+    return value;
+  }
 
-private:
-    std::deque<T> queue;
-    mutable std::mutex mutex;
-    size_t max_size;
-    std::condition_variable cv_not_full, cv_not_empty;
+  T front() {
+    std::unique_lock<std::mutex> lg{mutex};
+    cv_not_empty.wait(lg, [this]() { return queue.size() != 0; });
+    T value = queue.front();
+    return value;
+  }
+
+ private:
+  std::deque<T> queue;
+  mutable std::mutex mutex;
+  size_t max_size;
+  std::condition_variable cv_not_full, cv_not_empty;
 };
 
 #endif //AKSLAB5_CONCURRENT_QUEUE_H
